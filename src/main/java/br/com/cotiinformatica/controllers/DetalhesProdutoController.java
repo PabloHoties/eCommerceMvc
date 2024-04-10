bc77b010a334e56503359230dbb489a2a98395da
PabloHoties <33355184+PabloHoties@users.noreply.github.com>
2024-04-10 19:14:04 -0300

    feat: desenvolvimento models, views e controllers
    desenvolvimento de LojaProdutos, CarrinhoDeComprasModel, CarrinhoDeComprasController, ItemCarrinhoModel, DetalhesProdutoController

--- src/main/java/br/com/cotiinformatica/controllers/DetalhesProdutoController.java
+++ src/main/java/br/com/cotiinformatica/controllers/DetalhesProdutoController.java
@@ -1,17 +1,46 @@
 package br.com.cotiinformatica.controllers;
 
+import java.lang.reflect.Type;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import br.com.cotiinformatica.models.LojaProdutosModel;
 
 @Controller
 public class DetalhesProdutoController {
+	
+	//atributo
+	private String endpoint = "http://localhost:8083/api/produtos/obter";
 
 	@RequestMapping(value = "/detalhes-produto")
-	public ModelAndView detalhesProduto() {
+	public ModelAndView detalhesProduto(String id) {
 		// Definir a página que será exibida pelo controlador
 		// WEB-INF/views/detalhes-produto.jsp
 		ModelAndView modelAndView = new ModelAndView("detalhes-produto");
+		
+		try {
+			
+			RestTemplate restTemplate = new RestTemplate();
+			String dados = restTemplate.getForObject(endpoint + "/" + id, String.class);
+
+			// Desserializando o JSON retornado para um objeto
+			Gson gson = new Gson();
+			Type productListType = new TypeToken<LojaProdutosModel>() {
+			}.getType();
+			LojaProdutosModel produto = gson.fromJson(dados, productListType);
+			// Enviando os dados para a página
+			modelAndView.addObject("produto", produto);
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
 		return modelAndView;
 	}
 }
--- src/main/webapp/WEB-INF/views/loja-produtos.jsp
+++ src/main/webapp/WEB-INF/views/loja-produtos.jsp
@@ -2,6 +2,7 @@
     pageEncoding="ISO-8859-1"%>
     
 <%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>    
+<%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt" %>
     
 <!DOCTYPE html>
 <html>
@@ -25,19 +26,19 @@
 			Selecione o produto para incluí-lo no seu carrinho de compras.
 		</div>
 		
-		<div class="row">
+		<div class="row mt-3 mb-5">
 			<c:forEach items="${produtos}" var="p">
 				<div class="col-md-4">
 					<div class="card m-2">
 						<div class="card-body text-center">
 							
 							<h3>${p.nome}</h3>
-							<h5>Preço: ${p.preco}</h5>
+							<h5>Preço: <fmt:formatNumber value="${p.preco}" type="currency"/></h5>
 							
-							<img src="data:image/jpeg;base64, ${p.foto}" width="240" class="m-3"/>
+							<img src="data:image/jpeg;base64, ${p.foto}" height="200" class="m-3"/>
 							
 							<div class="mt-3 d-grid">
-								<a href="/eCommerceMvc/detalhes-produto" class="btn btn-primary">
+								<a href="/eCommerceMvc/detalhes-produto?id=${p.id}" class="btn btn-primary">
 									Adicionar ao carrinho
 								</a>
 							</div>
